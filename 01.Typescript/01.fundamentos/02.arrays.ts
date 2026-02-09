78bfde04f1c41fd3d5f4adf582e9e6ea1b8d9de5
JMHadga <jose.galdon12@gmail.com>
2026-02-09 14:47:00 +0100

    arrays

---
+++ 01.Typescript/01.fundamentos/02.arrays.ts
@@ -0,0 +1,20 @@
+//! Arrays o colecciones de datos.
+
+//* Hay dos tipos de sintaxis en los arrays.
+//? 1. Primera sintaxis, la más básica.
+const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+numbers.push(10);
+
+//? 2. Segunda sintaxis,<GENÉRICOS>, esta es más avanzada.
+const numbersAlts: Array<number> = [10, 20, 30];
+numbersAlts.push(40);
+
+//!Tupla, indicar LA LONGITUD Y TIPO DE DATO.
+const fruits: [string, string, string] = ["naranja", "mandarina", "uva"];
+
+//! Array vacío
+const empty = []; //* si es vacío es de tipo any(de cualquier tipo). EVITAR ANY SIEMPRE
+let strings: string[] = [];
+
+//! Arrays de tipos mixtos
+const mixto: (string | number)[] = ["typescript", 10];
